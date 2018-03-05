9fbfa1ab918cfb46d16312b679b2861f064c97e7
Tom Klein <tom@dwairyz.local>
2018-03-05 14:20:29 +0200

    Contract class

--- App_Code/Contract.cs
+++ App_Code/Contract.cs
@@ -8,6 +8,77 @@ using System.Web;
 /// </summary>
 public class Contract
 {
+    int contract_code;
+    int contype_id;
+    DateTime signature_sdate;
+    DateTime signature_fdate;
+    string contract_pic;
+
+    public int Contract_code
+    {
+        get
+        {
+            return contract_code;
+        }
+
+        set
+        {
+            contract_code = value;
+        }
+    }
+
+    public int Contype_id
+    {
+        get
+        {
+            return contype_id;
+        }
+
+        set
+        {
+            contype_id = value;
+        }
+    }
+
+    public DateTime Signature_sdate
+    {
+        get
+        {
+            return signature_sdate;
+        }
+
+        set
+        {
+            signature_sdate = value;
+        }
+    }
+
+    public DateTime Signature_fdate
+    {
+        get
+        {
+            return signature_fdate;
+        }
+
+        set
+        {
+            signature_fdate = value;
+        }
+    }
+
+    public string Contract_pic
+    {
+        get
+        {
+            return contract_pic;
+        }
+
+        set
+        {
+            contract_pic = value;
+        }
+    }
+
     public Contract()
     {
         //
