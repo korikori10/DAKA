34a3747970fa3361d739007a6890d12b13d31ea4
Tom <tolas22@gmail.com>
2018-03-07 14:36:50 +0200

    first webmethod

--- App_Code/AJAXWebService.cs
+++ App_Code/AJAXWebService.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
 using System.Web.Services;
 
 /// <summary>
@@ -22,9 +24,16 @@ public class AJAXWebService : System.Web.Services.WebService
     }
 
     [WebMethod]
-    public string HelloWorld()
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    public string getEmployees()
     {
-        return "Hello World";
+        Employee e = new Employee();
+        List<Employee> LE = e.getEmployees();
+        JavaScriptSerializer js = new JavaScriptSerializer();
+        // serialize to string
+        string jsonStringCategory = js.Serialize(LE);
+        return jsonStringCategory;
+        
     }
 
 }
