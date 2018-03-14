4e0cb29b06915b379048894f27004f90671c150f
Tom <tolas22@gmail.com>
2018-03-14 11:31:53 +0200

    bla bla

--- AjaxCalls.js
+++ AjaxCalls.js
@@ -5,11 +5,11 @@
         dataType: "json",
         contentType: 'application/json; charset = utf-8',
         success: function (results) {
-            console.log(results);
             renderEmployees(results);
         },
-        error: function (request, error) {
-            alert('Network error has occurred please try again!');
+        error: function (xhr, status, error) {
+            var err = eval("(" + xhr.responseText + ")");
+            alert(err.Message);
         }
        
     });
