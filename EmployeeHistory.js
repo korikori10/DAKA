39c1954fe0641f7674055c77bd1d78bc9bda2083
korikori10 <33927361+korikori10@users.noreply.github.com>
2018-04-06 21:04:16 +0300

    history,action
    employee history completed
    pdf,print,ecxel,copy done--need to be fix to our theme

--- EmployeeHistory.js
+++ EmployeeHistory.js
@@ -5,17 +5,17 @@ $(document).ready(function () {
     getHistory(EmployeeInfo)//, renderEmployeeByIDs
 });
 //
-function getHistory(EmployeeInfo, renderEmployeeByIDs) {
-    var dataString = JSON.stringify(EmployeeInfo);
+function getHistory(EmployeeInfo) {//, renderEmployeeByIDs
+  //  var dataString = JSON.stringify(EmployeeInfo);
 
     $.ajax({
         url: 'ajaxWebService.asmx/GetHistory',
-        data: dataString,
+        data: EmployeeInfo,
         type: 'POST',
         dataType: "json",
-        contentType: 'application/json; charset = utf-8',
+   //     contentType: 'application/json; charset = utf-8',
         success: function (data) {// renderEmployeeByIDs(results);
-          //  data = $.parseJSON(data.d);
+            //data = $.parseJSON(data.d);
             var datatableVariable = $('#HistoryTable').DataTable({
                 data: data,
                 columns: [
@@ -36,45 +36,14 @@ function getHistory(EmployeeInfo, renderEmployeeByIDs) {
                             var month = date.getMonth() + 1;
                             return date.getDate() + "/" + month + "/" + date.getFullYear();
                         }
-                    },]
+                    }
+                ]
             });
 
 
         
-        },
-        //error: function (xhr, status, error) {
-        //    var err = eval("(" + xhr.responseText + ")");
-        //    alert(err.Message);
-        //    console.log(err.Message)
-        //}
+        }
     });
     
 }
-    
-function renderEmployeeByIDs(data) {
-    data = $.parseJSON(data.d);
-    var datatableVariable = $('#HistoryTable').DataTable({
-        data: data,
-        columns: [
-            { 'data': 'Employee_pass_id' },
-            { 'data': 'Fname' },
-            { 'data': 'Lname' },
-            { 'data': 'Bus_name' },
-            {
-                'data': 'Start_date', 'render': function (date) {
-                    var date = new Date(parseInt(date.substr(6)));
-                    var month = date.getMonth() + 1;
-                    return date.getDate() + "/" + month + "/" + date.getFullYear();
-                }
-            },
-            {
-                'data': 'End_date', 'render': function (date) {
-                    var date = new Date(parseInt(date.substr(6)));
-                    var month = date.getMonth() + 1;
-                    return date.getDate() + "/" + month + "/" + date.getFullYear();
-                }
-            },]
-    });
-
-
-}
+ 
