1096d3ccc82ccddb769f857bb8b81760cc558c03
Tom <tolas22@gmail.com>
2018-06-17 18:28:48 +0300

    users

---
+++ jsDAKA/Users.js
@@ -0,0 +1,29 @@
+﻿UserInfo = new Object();
+
+
+window.onload = function () {
+    getUsers();
+}
+
+// Button Clicks In Tables
+$('.table').on('click', 'tr td button', function () {
+    //Take the Employee ID from the table row
+    sessionStorage.removeItem("empInfo")
+    tr = $(this).closest('tr');//.find('td:first').text();
+    tableId = $(this).closest('.table').attr('id');
+    whichid = $(this).closest('button').attr('name');
+    var data = $("#" + tableId).DataTable().row(tr).data();
+    EmployeeInfo.pass = data['Employee_pass_id'];
+    if (whichid == "edit") {
+        //Go To Employee Page
+        sessionStorage.setItem("empInfo", EmployeeInfo.pass);
+        window.location = "Employee.html";
+    }
+
+
+    else {
+        //Send SMS To Employees
+        // SendSMS();
+        window.location = "error404.html";
+    }
+});
