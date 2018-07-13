fedc59113bcb5e70f26674f1d567ada133ec5e63
Tom <tolas22@gmail.com>
2018-07-13 16:58:53 +0300

    hope nothing got deleted

--- App_Code/PDF.cs
+++ App_Code/PDF.cs
@@ -169,18 +169,28 @@ public class PDF
                 {
                     var reader = new PdfReader(inputPdfStream);
                     var stamper = new PdfStamper(reader, outputPdfStream);
-                    var pdfContentByte = stamper.GetOverContent(1);
-
                     AcroFields formFields = stamper.AcroFields;
                     if (formFields.Fields.ContainsKey("signature"))
                     {
-                    AcroFields.FieldPosition f = formFields.GetFieldPositions("signature")[0];
-                    formFields.SetField("Start_date", DateTime.Now.ToString("yyyy/MM/dd"));
-                    iTextSharp.text.Rectangle rect = f.position;
-                    itextImage.ScaleToFit(rect.Width, rect.Height);
-                    itextImage.SetAbsolutePosition(rect.Left, rect.Bottom);
+                        int i = 0;
+                        foreach (var field in formFields.Fields)
+                        {
+                            if (field.Key == "signature")
+                            {
+                                AcroFields.FieldPosition f = formFields.GetFieldPositions(field.Key)[i];
+                                int page = formFields.GetFieldPositions(field.Key)[i].page;//get(i).page;
+                                formFields.SetField("Start_date", DateTime.Now.ToString("yyyy/MM/dd"));
+                                iTextSharp.text.Rectangle rect = f.position;
+                                itextImage.ScaleToFit(rect.Width, rect.Height);
+                                int p = reader.NumberOfPages;
 
-                    pdfContentByte.AddImage(itextImage);
+                                    itextImage.SetAbsolutePosition(rect.Left, rect.Bottom);
+                                i++;
+                                var pdfContentByte = stamper.GetOverContent(page);
+                                pdfContentByte.AddImage(itextImage);
+
+                            }
+                        }
 
                     }
                     stamper.FormFlattening = true;
